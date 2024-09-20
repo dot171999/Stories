2957ea62f14053c328adfc1b9622ef2a75336453
Aryan Sharma <agsharma98@gmail.com>
2024-09-19 23:35:29 -0700

    Initial Commit

---
+++ Stories/StoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoriesApp.swift
+//  Stories
+//
+//  Created by Aryan Sharma on 19/09/24.
+//
+
+import SwiftUI
+
+@main
+struct StoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
